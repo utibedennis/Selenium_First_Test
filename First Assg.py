4a1ee4b471581f96937417592bdcf48293bccb7f
utibedennis <utibe.dennis@gmail.com>
2025-03-19 18:19:51 +0100

    Updating to add my first assignment

--- First Assg.py
+++ First Assg.py
@@ -1,7 +1,9 @@
 import time
+from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
 
 driver = webdriver.Chrome()
 driver.get("https://www.saucedemo.com/")
@@ -9,6 +11,7 @@ driver.maximize_window()
 driver.find_element(By.ID, "user-name").send_keys("standard_user")
 driver.find_element(By.ID, "password").send_keys("secret_sauce")
 driver.find_element(By.ID, "login-button").click()
+time.sleep(5)
 
 driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
 driver.find_element(By.ID, "add-to-cart-sauce-labs-bike-light").click()
@@ -16,15 +19,14 @@ driver.find_element(By.ID, "add-to-cart-sauce-labs-bolt-t-shirt").click()
 driver.find_element(By.ID, "add-to-cart-sauce-labs-onesie").click()
 driver.find_element(By.ID, "add-to-cart-test.allthethings()-t-shirt-(red)").click()
 driver.find_element(By.ID, "add-to-cart-sauce-labs-fleece-jacket").click()
-time.sleep(10)
 driver.find_element(By.ID, "shopping_cart_container").click()
-driver.find_element(By.ID, "checkout").click()
-time.sleep(10)
+
 driver.find_element(By.ID, "first-name").send_keys("Uti")
 driver.find_element(By.ID, "last-name").send_keys("Ike")
 driver.find_element(By.ID, "postal-code").send_keys("223045")
 driver.find_element(By.ID, "continue").click()
 driver.find_element(By.ID, "finish").click()
+driver.find_element(By.ID, "back-to-products").click()
+time.sleep(5)
 
 
-time.sleep(10)
