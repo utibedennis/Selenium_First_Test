e3469db96b5611098f5a98a2cd4a218bad599182
utibedennis <utibe.dennis@gmail.com>
2025-03-23 20:32:38 +0100

    Readme files and requirement.txt

--- First Assg.py
+++ First Assg.py
@@ -1,32 +1,64 @@
 import time
 from time import sleep
 
+from pycparser.ply.yacc import format_stack_entry
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+#Variable declaration
+wait = 5
+url = "https://www.saucedemo.com/"
+username = "standard_user"
+password = "secret_sauce"
+firstname = "Uti"
+lastname =  "Ike"
+postalcode =  "223045"
 
+#Browser initialization
 driver = webdriver.Chrome()
-driver.get("https://www.saucedemo.com/")
+driver.get(url)
 driver.maximize_window()
-driver.find_element(By.ID, "user-name").send_keys("standard_user")
-driver.find_element(By.ID, "password").send_keys("secret_sauce")
-driver.find_element(By.ID, "login-button").click()
-time.sleep(5)
 
-driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
-driver.find_element(By.ID, "add-to-cart-sauce-labs-bike-light").click()
-driver.find_element(By.ID, "add-to-cart-sauce-labs-bolt-t-shirt").click()
-driver.find_element(By.ID, "add-to-cart-sauce-labs-onesie").click()
-driver.find_element(By.ID, "add-to-cart-test.allthethings()-t-shirt-(red)").click()
-driver.find_element(By.ID, "add-to-cart-sauce-labs-fleece-jacket").click()
-driver.find_element(By.ID, "shopping_cart_container").click()
+#Login Details
+UserName =driver.find_element(By.ID, "user-name")
+UserName.send_keys("standard_user")
 
+PassWord =driver.find_element(By.ID, "password")
+Password.send_keys("secret_sauce")
+
+Login = driver.find_element(By.ID, "login-button")
+Login.click()
+time.sleep(wait)
+
+#Add to Cart
+Backpack =driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack")
+Backpack.click()
+
+BikeLight =driver.find_element(By.ID, "add-to-cart-sauce-labs-bike-light")
+BikeLight.click()
+
+Tshirt =driver.find_element(By.ID, "add-to-cart-sauce-labs-bolt-t-shirt")
+Tshirt.click()
+
+onesie =driver.find_element(By.ID, "add-to-cart-sauce-labs-onesie")
+onesie.click()
+
+Redshirt =driver.find_element(By.ID, "add-to-cart-test.allthethings()-t-shirt-(red)")
+Redshirt.click()
+
+Jacket =driver.find_element(By.ID, "add-to-cart-sauce-labs-fleece-jacket")
+Jacket.click()
+
+Cart_container =driver.find_element(By.ID, "shopping_cart_container")
+Cart_container.click()
+
+#Checkout details
 driver.find_element(By.ID, "first-name").send_keys("Uti")
 driver.find_element(By.ID, "last-name").send_keys("Ike")
 driver.find_element(By.ID, "postal-code").send_keys("223045")
 driver.find_element(By.ID, "continue").click()
 driver.find_element(By.ID, "finish").click()
 driver.find_element(By.ID, "back-to-products").click()
-time.sleep(5)
+time.sleep(wait)
 
 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Selenium_First_Test
+I segmented my code to give clarity.
+I also put my data into variable for ease of location and changes
